98cad08dc0b2dc265d42262dc1f162bb77c29b1f
Emil <intestinal_d@rambler.ru>
2022-09-14 16:01:14 +0300

    2.1.3 - Changed enum field in one model

--- Events/AccountCreatedEvent.cs
+++ Events/AccountCreatedEvent.cs
@@ -5,7 +5,7 @@ namespace IncredibleBackendContracts.Events
     public class AccountCreatedEvent
     {
         public int Id { get; set; }
-        public Currency Currency { get; set; }
+        public TradingCurrency Currency { get; set; }
         public AccountStatus Status { get; set; }
         public int LeadId { get; set; }
     }
